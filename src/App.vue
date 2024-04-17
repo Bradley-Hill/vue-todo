b58cc6d2cbd7b728312c6b16dcf25b6b005eca65
Bradley Hill <brad.michael.hill@gmail.com>
2024-04-17 09:46:09 +0200

    Adding some scripting

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
 
-<template></template>
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_ascend = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+</script>
+
+<template>
+  <div>Hello, World</div>
+</template>
