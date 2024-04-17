0333b22b96e167301de5087c03e7c77094a2bc68
Bradley Hill <brad.michael.hill@gmail.com>
2024-04-17 09:52:44 +0200

    added onMounted function for pulling name from localStorage

--- src/App.vue
+++ src/App.vue
@@ -12,8 +12,22 @@ const todos_ascend = computed(() =>
     return b.createdAt - a.createdAt;
   })
 );
+
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+});
 </script>
 
 <template>
-  <div>Hello, World</div>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Whats up, <input type="text" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+  </main>
 </template>
